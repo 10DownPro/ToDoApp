e419b7b76b0b4670eb460df186ab9c46d4694f63
Shelby Henry <shenry0913@gmail.com>
2022-10-13 19:54:43 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-
+const path = require('path');
 const express = require("express");
+const layout = require('express-layout');
 
 const app = express();
 
-const port = 3000;
+const bodyParser = require('body-parser');
+
+const port = 3001;
+
+app.set('views' , path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const middlewares = [
+    layout(),
+    express.static(path.join(__dirname, 'public')),
+    bodyParser.urlencoded({extended: false})
+]
+
+app.use(middlewares);
 
 
 app.use(require('./routers/authentication'));
@@ -12,6 +26,11 @@ app.use(require('./routers/naomi'));
 app.use(require('./routers/shelby'));
 app.use(require('./routers/tvedt'));
 
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>This is where you register!</h1>');
+});
 
 
 app.listen(port, () => {
